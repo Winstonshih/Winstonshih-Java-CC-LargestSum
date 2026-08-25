1e7333c2124cc789af53d5d3c1956e468d635061
Winstonshih <Winstonshih@github.com>
2026-08-25 17:42:45 +0000

    Test run: 2/4 passed, 2 failed, 0 skipped - 2026-08-25T17:42:45.080Z

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -11,7 +11,7 @@ public class LargestSum {
      */
     public int bigSum(List<Integer> nums){
         int sum=0;
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<nums.size()-1;i++)
         {
             for(int j=i+1;j<nums.size();j++)
             {
